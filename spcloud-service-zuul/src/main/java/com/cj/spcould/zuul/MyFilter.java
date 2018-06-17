351b83edff8c12d9c03197c7ccd11ca19569a661
chenjun <quick_to_bowl@163.com>
2018-06-17 15:50:10 +0800

    路由网关Zuul

---
+++ spcloud-service-zuul/src/main/java/com/cj/spcould/zuul/MyFilter.java
@@ -0,0 +1,60 @@
+package com.cj.spcould.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * ZuulFilter过滤器
+ *
+ * @author <a href="mailto:cjun@zjport.gov.cn">cjun</a>
+ * @version $Id$
+ * @since 1.0
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+
+    /** Logger日志 */
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+            log.warn("token is empty");
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(401);
+            try {
+                requestContext.getResponse().getWriter().write("token is empty");
+            } catch (Exception e) {
+            }
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
