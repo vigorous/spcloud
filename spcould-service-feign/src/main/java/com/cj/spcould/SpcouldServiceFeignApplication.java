3cb4d84b80c56c66681170b650e74e2e48bfdc40
chenjun <quick_to_bowl@163.com>
2018-06-17 12:18:02 +0800

    改造service-feign（断路器：Hystrix 仪表盘）

--- spcould-service-feign/src/main/java/com/cj/spcould/SpcouldServiceFeignApplication.java
+++ spcould-service-feign/src/main/java/com/cj/spcould/SpcouldServiceFeignApplication.java
@@ -1,16 +1,31 @@
 package com.cj.spcould;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients////打开Feign注解
 //@EnableCircuitBreaker//打开Hystrix断路器
 //@ServletComponentScan//扫描缓存
+@EnableHystrixDashboard
 public class SpcouldServiceFeignApplication {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpcouldServiceFeignApplication.class, args);
