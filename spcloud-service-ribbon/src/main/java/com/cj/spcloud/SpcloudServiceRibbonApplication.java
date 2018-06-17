481558e41d84e0f90fd6f33a6b964a8aca0138d7
chenjun <quick_to_bowl@163.com>
2018-06-17 12:17:30 +0800

    改造service-ribbon（断路器：Hystrix 仪表盘）

--- spcloud-service-ribbon/src/main/java/com/cj/spcloud/SpcloudServiceRibbonApplication.java
+++ spcloud-service-ribbon/src/main/java/com/cj/spcloud/SpcloudServiceRibbonApplication.java
@@ -1,10 +1,13 @@
 package com.cj.spcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +18,8 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableHystrix
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class SpcloudServiceRibbonApplication {
 
     /**
@@ -27,6 +31,16 @@ public class SpcloudServiceRibbonApplication {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
     /**
